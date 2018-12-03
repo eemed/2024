e0ef5ae926c6182762705b51bcd90beaed7228ab
eemed <eemed@protonmail.com>
2018-12-03 10:40:13 +0200

    grid functions

--- Grid.js
+++ Grid.js
@@ -1,5 +1,4 @@
 const direction = { "up": 1, "down": 2, "left": 3, "right": 4 };
-Object.freeze(direction);
 
 class Grid {
   constructor(size) {
@@ -22,6 +21,27 @@ class Grid {
     console.log(this.matrix);
   }
 
+  insertTile(x, y, value) {
+    matrix[x][y] = new Tile(x, y, value);
+  }
+
+  removeTile(x, y) {
+    matrix[x][y] = null;
+  }
+
+  eachTile(callback) {
+    for (let x = 0; x < this.size; x += 1) {
+      for (let y = 0; y < this.size; y += 1) {
+        callback(x, y, this.matrix[x][y]);
+      }
+    }
+  }
+
+  getRandomUnusedTile() {
+    const unusedPositions = this.unusedTiles;
+    return unusedPosition[Math.floor(Math.random() * unusedPositions.length)];
+  }
+
   unusedTiles() {
     const positions = [];
 
@@ -31,14 +51,6 @@ class Grid {
       }
     });
     return positions;
-  }
-
-  eachTile(callback) {
-    for (let x = 0; x < this.size; x += 1) {
-      for (let y = 0; y < this.size; y += 1) {
-        callback(x, y, this.matrix[x][y]);
-      }
-    }
   }
 }
 const grid = new Grid(4);
