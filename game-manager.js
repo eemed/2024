158feef59eeb0cca45f961a7cfa5ee448dee859f
eemed <eemed@protonmail.com>
2018-12-03 11:32:47 +0200

    game manager

--- game-manager.js
+++ game-manager.js
@@ -5,5 +5,23 @@ class GameManager {
     this.won = false;
     this.inputManager = inputManager;
     this.renderer = renderer;
+
+    // Pass in methods from this class
+    this.inputManager.on('move', this.onMove.bind(this));
+    this.inputManager.on('restart', this.onRestart.bind(this));
+  }
+
+  onMove() {
+  }
+
+  onRestart() {
+  }
+
+  addRandomTile() {
+    if (!this.grid.isFull()) {
+      let value = Math.random() < 0.9 ? 2 : 4;
+      let tile = new Tile(this.getRandomUnusedTile(), value);
+      this.grid.insertTile(tile);
+    }
   }
 }
