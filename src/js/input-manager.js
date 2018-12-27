db30174aef2a6599c4d4b8966a2b2aef51f97043
eemed <eemed@protonmail.com>
2018-12-27 21:10:19 +0200

    initial keymapping for movement

--- src/js/input-manager.js
+++ src/js/input-manager.js
@@ -1,7 +1,47 @@
+import { DIRECTION } from './grid';
+export const EVENTS = { 'MOVE': 'move', 'RESTART': 'restart' };
+
 export default class InputManager {
   constructor() {
+    this.events = {};
+    this.onEvent.bind(this);
+    this.addEventListeners();
+    console.log(this.events)
+  }
+
+  on(eventName, callback) {
+    this.events[eventName] = callback;
+  }
+
+  onEvent(event, events) {
+    switch (event.key) {
+      case 'ArrowUp':
+      case 'w':
+        events[EVENTS.MOVE](DIRECTION.UP);
+        break;
+      case 'ArrowDown':
+      case 's':
+        events[EVENTS.MOVE](DIRECTION.DOWN);
+        break;
+      case 'ArrowRight':
+      case 'd':
+        events[EVENTS.MOVE](DIRECTION.RIGHT);
+        break;
+      case 'ArrowLeft':
+      case 'a':
+        events[EVENTS.MOVE](DIRECTION.LEFT);
+        break;
+      default:
+        break;
+    }
   }
 
   addEventListeners() {
+    document.addEventListener('keydown', (e) => this.onEvent(e, this.events));
   }
 }
+
+// Javascript keycode for l: 76
+// Javascript keycode for k: 75
+// Javascript keycode for j: 74
+// Javascript keycode for h: 72
