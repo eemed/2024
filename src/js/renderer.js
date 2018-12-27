cad9fbff1cdee90666119401a0e3f319066f6028
eemed <eemed@protonmail.com>
2018-12-27 21:10:31 +0200

    render method

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,6 +1,17 @@
 export default class Renderer {
-  constructor() {
+  constructor(grid) {
+    this.grid = grid;
     this.scoreDisplay = document.querySelector('.score-display');
     this.tileArea = document.querySelector('.tile-area');
+  }
+
+  render() {
+    let values = document.querySelectorAll('.value');
+
+    if (values.length !== this.grid.size ** 2) return;
+
+    for (let i = 0; i < values.length; ++i) {
+      values[i].innerHTML = this.grid.getValue(i);
+    }
   }
 }
