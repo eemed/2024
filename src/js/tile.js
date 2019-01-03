a9d61695ce4bad17f2e26925465621acdedb8ea7
eemed <eemed@protonmail.com>
2019-01-03 22:31:08 +0200

    add data to tile

--- src/js/tile.js
+++ src/js/tile.js
@@ -9,11 +9,42 @@ export class Position {
   }
 }
 
+export function getColor(value) {
+  switch (value) {
+    case 2:
+      return "#100e23"
+    case 4:
+      return "#565575"
+    case 8:
+      return "#ffe9aa"
+    case 16:
+      return "#ff8080"
+    case 32:
+      return "##95ffa4"
+    case 64:
+      return "#91ddff"
+    case 128:
+      return "#c991e1"
+    case 256:
+      return "#aaffe4"
+    case 512:
+      return "#cbe3e7"
+    case 1024:
+      return "#ff5458"
+    case 2048:
+      return "#65b2ff"
+    default:
+      return "#a6b3cc"
+  }
+}
+
 export default class Tile {
-  constructor(pos, value) {
+  constructor(pos, value, identifier) {
+    this.identifier = identifier;
     this.position = pos;
-    this.prevPosition = new Position(pos.x, pos.y);
+    this.prevPosition = null;
     this.value = value;
+    this.mergedFrom = [];
   }
 
   isWinner() {
@@ -24,8 +55,23 @@ export default class Tile {
     return {
       position: this.position.serialize(),
       prevPosition: this.prevPosition.serialize(),
-      value: this.value
+      value: this.value,
     };
+  }
+
+  setMergedFrom(tile, otherTile) {
+    if (this.mergedFrom.length > 2) { return; }
+
+    this.mergedFrom.push(tile);
+    this.mergedFrom.push(otherTile);
+  }
+
+  clearMergedFrom() {
+    this.mergedFrom = [];
+  }
+
+  isMerged() {
+    return this.mergedFrom.length > 0;
   }
 
   updatePosition(pos) {
