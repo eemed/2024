61fd0403bdf1bbe7049dfdae25096427f424ee02
eemed <eemed@protonmail.com>
2019-01-04 15:16:41 +0200

    tile render

--- src/js/tile.js
+++ src/js/tile.js
@@ -1,5 +1,8 @@
 const CSS_TILE_CLASS = 'tile';
 const CSS_VALUE_CLASS = 'value';
+const TILE_SIZE = 90;
+const TILE_GAP = 10;
+const TILE_TOTAL = TILE_SIZE + TILE_GAP;
 
 export class Position {
   constructor(x, y) {
@@ -9,6 +12,16 @@ export class Position {
 
   serialize() {
     return { x: this.x, y: this.y };
+  }
+
+  clone() {
+    return new Position(this.x, this.y);
+  }
+
+  multiply(multiplier) {
+    this.x *= multiplier;
+    this.y *= multiplier;
+    return this;
   }
 }
 
@@ -48,14 +61,17 @@ export default class Tile {
     this.value = value;
     this.mergedFrom = [];
     this.html = this.createHTMLTile(parentElement);
+
+    this.instantRender();
   }
 
   createHTMLTile(parentElement) {
+    console.log('create')
     let tile = document.createElement('div');
-    tile.className = 'tile';
+    tile.className = CSS_TILE_CLASS;
 
     let value = document.createElement('p');
-    value.className = 'value';
+    value.className = CSS_VALUE_CLASS;
     value.innerHTML = this.value;
 
     tile.appendChild(value);
@@ -64,15 +80,50 @@ export default class Tile {
   }
 
   render() {
-    setHTMLValue();
-    if (prevPosition !== null) {
-      // TODO implement animate
-      animate();
+    this.setHTMLValue();
+
+    if (this.prevPosition != null) {
+      if ( this.prevPosition.x !== this.position.x ||
+        this.prevPosition.y !== this.position.y) {
+
+        this.animate();
+      }
     }
   }
 
+  animate() {
+    let id = setInterval(frame, 5, this);
+
+    let from = this.prevPosition.clone().multiply(TILE_TOTAL);
+    let to = this.position.clone().multiply(TILE_TOTAL);
+
+    function frame(tile) {
+      if (from.x === to.x && from.y === to.y) {
+        clearInterval(id);
+        tile.prevPosition = null;
+      } else if (from.x > to.x) {
+        from.x -= 1;
+      } else if (from.x < to.x) {
+        from.x += 1;
+      } else if (from.y > to.y) {
+        from.y -= 1;
+      } else if (from.y < to.y) {
+        from.y += 1;
+      }
+
+      tile.html.style.top = from.y + "px";
+      tile.html.style.left = from.x + "px";
+    }
+  }
+
+
+  instantRender() {
+    this.html.style.left = this.position.x * (TILE_TOTAL) + "px";
+    this.html.style.top  = this.position.y * (TILE_TOTAL) + "px";
+  }
+
   setHTMLValue() {
-    this.html.querySelector('value').innerHTML = this.value;
+    this.html.querySelector('.value').innerHTML = this.value;
   }
 
   isWinner() {
@@ -103,6 +154,8 @@ export default class Tile {
   }
 
   updatePosition(pos) {
+    console.log('update Position', this.position, pos)
+    this.prevPosition = this.position;
     this.position = pos;
   }
 }
