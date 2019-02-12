f0af4d98bd7ee6fada73c15aaa6f9369779cb420
eemed <eemed@protonmail.com>
2019-02-12 23:35:03 +0200

    export controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,13 @@
-const highScoreModel = require('../models/high-scores');
+const HighScoreModel = require('../models/high-scores');
+
+function getTop10() {
+  return 0;
+}
 
 function index(req, res) {
   res.status(200);
   res.header('Content-Type', 'text/html');
   res.render('index');
 }
+
+module.exports = index;
