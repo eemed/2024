8a42e732c27dc4411c9ccfbd06659d233ce9c3d3
eemed <eemed@protonmail.com>
2019-02-12 23:34:35 +0200

    hbs and static content

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
+const hbs = require('express-hbs');
+const path = require('path');
 const index = require('./controllers/index');
 
 const port = 3000;
 const app = express();
 
 app.use(helmet());
+
+app.engine('hbs', hbs.express4());
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
+
+app.use('/static', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
   index(req, res);
