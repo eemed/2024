54c90331b7d31b25ab0665c46d796a1ce46597ff
eemed <eemed@protonmail.com>
2019-02-12 23:43:07 +0200

    render from db

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,15 @@
 const HighScoreModel = require('../models/high-scores');
 
-function getTop10() {
-  return 0;
-}
-
 function index(req, res) {
-  res.status(200);
-  res.header('Content-Type', 'text/html');
-  res.render('index');
+  HighScoreModel.find((err, highscores) => {
+    if (err) return console.error(err);
+
+    console.log(highscores);
+    res.status(200);
+    res.header('Content-Type', 'text/html');
+    res.render('index', { scores: highscores });
+    return console.log('Found items');
+  });
 }
 
 module.exports = index;
